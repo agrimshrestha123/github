edb50269523593309318fb111a33492eb63babc6
agrimshrestha123 <agrimstha3@gmail.com>
2025-05-18 12:06:20 +0545

    newscard

--- flutter_application_2/lib/main.dart
+++ flutter_application_2/lib/main.dart
@@ -10,34 +10,48 @@ void main() {
           title: Text('Trending'),
           actions: [Icon(Icons.more_vert)],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                "images/this.jpg",
-                width: double.infinity,
-                height: 300,
-                fit: BoxFit.cover,
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Russian Warship: Moskva sinks in Black Sea',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-              ),
-              Row(
-                children: [
-                  Icon(Icons.schedule),
-                  Text('4hr ago'),
-                  SizedBox(width: 10),
-                ],
-              ),
-            ],
-          ),
+        body: SingleChildScrollView(
+          child: Column(children: [NewsCard(), NewsCard(), NewsCard()]),
         ),
       ),
     ),
   );
+}
+
+class NewsCard extends StatelessWidget {
+  const NewsCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Image.asset(
+              "images/this.jpg",
+              width: double.infinity,
+              height: 200,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Russian Warship: Moskva sinks in Black Sea',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+          ),
+          Row(
+            children: [
+              Icon(Icons.schedule),
+              Text('4hr ago'),
+              SizedBox(width: 10),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
 //60
